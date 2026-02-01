7fcc78210556b3d61e5d8e6df0279d3e751478c3
Julian Papesch <jpapesch000@gmail.com>
2026-02-01 12:16:29 +0100

    Button Pressed and Unpressed Logic

--- GGJ26/Assets/Scripts/Gameplay/PushButton.cs
+++ GGJ26/Assets/Scripts/Gameplay/PushButton.cs
@@ -4,15 +4,30 @@ using UnityEngine.Events;
 public class PushButton : MonoBehaviour, ITongueable
 {
     [SerializeField] private UnityEvent OnPushed;
-    private TongueData tongueData;
-    private Vector3 hitPoint;
+    [SerializeField] private GameObject pressed;
+    [SerializeField] private GameObject unpressed;
     public void OnTongued(Transform tongueOrigin, Transform playerTransform, TongueData tongueData, Vector3 hitPoint)
     {
-        this.tongueData = tongueData;
-        this.hitPoint = hitPoint;
-
         OnPushed?.Invoke();
 
-        Timers.UntilThen(Constants.TONGUE_Speed / 2, () => { tongueData.StayAttached(hitPoint); }, () => { tongueData.ResetTongue(); });
+
+        TogglePressed(true);
+        Timers.UntilThen(Constants.TONGUE_Speed / 2, () => 
+        { 
+            tongueData.StayAttached(hitPoint); 
+        }, () => 
+        { 
+            tongueData.ResetTongue(); 
+            TogglePressed(false);
+        }); 
+    }
+    void Start()
+    {
+        TogglePressed(false);
+    }
+    void TogglePressed(bool state)
+    {
+        pressed.SetActive(state);
+        unpressed.SetActive(!state);
     }
 }
